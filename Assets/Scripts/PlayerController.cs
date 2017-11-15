da59929e1109648b54077b995d7a74e037ed0037
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-11-14 21:33:02 -0500

    Removed SetupLocalPlayer.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,36 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
 
     public float walkSpeed = 5.0f;//how fast the player walks
-
-    public GameObject bulletPrefab;//the prefab for the bullet
 
     private Rigidbody2D rb2d;
 
     // Use this for initialization
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        if (isLocalPlayer)
+        {
+            Camera.main.GetComponent<CameraController>().player = gameObject;
+            rb2d = GetComponent<Rigidbody2D>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        if (horizontal != 0 || vertical != 0)
+        if (isLocalPlayer)
         {
-            float terrainMultiplier = LevelManager.getTile(transform.position).terrainSpeedMultiplier;
-            Vector2 direction = (terrainMultiplier * walkSpeed * horizontal * Vector3.right) + (terrainMultiplier * walkSpeed * vertical * Vector3.up);
-            rb2d.velocity = direction;
-        }
-        else
-        {
-            rb2d.velocity = Vector2.zero;
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            if (horizontal != 0 || vertical != 0)
+            {
+                float terrainMultiplier = 1;// LevelManager.getTile(transform.position).terrainSpeedMultiplier;
+                Vector2 direction = (terrainMultiplier * walkSpeed * horizontal * Vector3.right) + (terrainMultiplier * walkSpeed * vertical * Vector3.up);
+                rb2d.velocity = direction;
+            }
+            else
+            {
+                rb2d.velocity = Vector2.zero;
+            }
         }
     }
 }
