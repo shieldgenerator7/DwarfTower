6adec29cb6221e645a396629e4edf46319250521
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-07-02 00:45:02 -0400

    Turret no longer targets enemies

--- Assets/Scripts/Characters/Drawf/TurretController.cs
+++ Assets/Scripts/Characters/Drawf/TurretController.cs
@@ -4,26 +4,16 @@ using UnityEngine;
 
 public class TurretController : MonoBehaviour {
 
-    public float detectionRange = 10.0f;//how close enemies must be in order for the turret to activate
     public GameObject owner;//the player who placed the turret
     private Vector2 direction;
 
-    private List<GameObject> enemies;
     private GunController gunController;
 
-	// Use this for initialization
-	void Start () {
-        enemies = new List<GameObject>();
-        foreach (PlayerController pc in FindObjectsOfType<PlayerController>())
-        {
-            GameObject go = pc.gameObject;
-            if (go != owner)
-            {
-                enemies.Add(go);
-            }
-        }
+    // Use this for initialization
+    void Start()
+    {
         gunController = GetComponent<GunController>();
-	}
+    }
 
     public void playerFired(Vector2 start, Vector2 target)
     {
