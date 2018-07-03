a34459140a77d3ae85d37d0f27282a3220301096
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-07-03 01:23:31 -0400

    Getting stunned grants stun resistance

--- Assets/Scripts/Characters/Stunnable.cs
+++ Assets/Scripts/Characters/Stunnable.cs
@@ -9,8 +9,36 @@ public class Stunnable : NetworkBehaviour
     public float stunResistancePercent = 0;
     [Range(0, 1)]
     public float knockbackResistancePercent = 0;
+    [Range(0,10)]
     public float postStunInvulnerabeDuration = 1;//how many seconds of invulnerability you get after being stunned
+
+    public float StunResistance
+    {
+        get { return stunResistancePercent + stunResistanceBonus; }
+        private set { }
+    }
+    public float KnockbackResistance
+    {
+        get { return knockbackResistancePercent + knockbackResistanceBonus; }
+        private set { }
+    }
+
+    [Header("Adaptability")]
+    [Range(0, 1)]
+    public float stunResistanceIncrement = 0.1f;
+    [Range(0, 1)]
+    public float knockbackResistanceIncrement = 0.1f;
+    [Header("Adaptability Decay")]
+    [Range(0, 1)]
+    public float stunResistanceDecayPerSecond = 0.01f;
+    [Range(0, 1)]
+    public float knockbackResistanceDecayPerSecond = 0.01f;
     
+    [SyncVar]
+    public float stunResistanceBonus = 0;
+    [SyncVar]
+    public float knockbackResistanceBonus = 0;
+
     public bool Stunned
     {
         get { return Time.time < stunStartTime + stunDuration; }
@@ -34,14 +62,28 @@ public class Stunnable : NetworkBehaviour
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        EventOnUnstunned += adapt;
     }
 
     private void Update()
     {
         if (Stunned)
         {
-            stunDuration = Mathf.MoveTowards(stunDuration, stunDuration * (1 - stunResistancePercent), Time.deltaTime);
-            rb2d.velocity = knockbackDirection * (knockbackSpeed * (1 - knockbackResistancePercent));
+            stunDuration = Mathf.MoveTowards(stunDuration, stunDuration * (1 - StunResistance), Time.deltaTime);
+            rb2d.velocity = knockbackDirection * (knockbackSpeed * (1 - KnockbackResistance));
+        }
+        //if just came out of stunned
+        else if (Time.time - Time.deltaTime < stunStartTime + stunDuration)
+        {
+            CmdUnstun();
+        }
+        //just regular walking around, not stunned
+        else
+        {
+            stunResistanceBonus -= stunResistanceDecayPerSecond * Time.deltaTime;
+            stunResistanceBonus = Mathf.Clamp(stunResistanceBonus, 0, 1);
+            knockbackResistanceBonus -= knockbackResistanceDecayPerSecond * Time.deltaTime;
+            knockbackResistanceBonus = Mathf.Clamp(knockbackResistanceBonus, 0, 1);
         }
     }
 
@@ -61,5 +103,29 @@ public class Stunnable : NetworkBehaviour
         }
     }
 
+    [Command]
+    public void CmdUnstun()
+    {
+        if (EventOnUnstunned != null)
+        {
+            EventOnUnstunned();
+        }
+    }
+
+    /// <summary>
+    /// When coming out of stun, gain stun resistance
+    /// </summary>
+    public void adapt()
+    {
+        stunResistanceBonus += stunResistanceIncrement;
+        knockbackResistanceBonus += knockbackResistanceIncrement;
+    }
+
     public delegate void OnStunned(float duration, float knockbackSpeed);
+    [SyncEvent]
+    public event OnStunned EventOnStunned;
+
+    public delegate void OnUnstunned();
+    [SyncEvent]
+    public  event OnUnstunned EventOnUnstunned;
 }
