3d243a20480f61f612a00d3771d3c29961cce2b8
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-07-04 01:29:10 -0400

    Crosshair changes based on mana level

--- Assets/CrosshairController.cs
+++ Assets/CrosshairController.cs
@@ -10,9 +10,21 @@ public class CrosshairController : MonoBehaviour
     public Image image;
     public Canvas canvas;
 
+    private ManaPool mana;
+
     // Use this for initialization
     void Start()
     {
+        if (image == null)
+        {
+            image = GetComponent<Image>();
+        }
+    }
+
+    public void init(GameObject player)
+    {
+        this.player = player;
+        mana = player.GetComponent<ManaPool>();
     }
 
     // Update is called once per frame
@@ -21,5 +33,22 @@ public class CrosshairController : MonoBehaviour
         Vector2 viewPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
         transform.position = new Vector2(viewPos.x * canvas.pixelRect.width,
             viewPos.y * canvas.pixelRect.height);
+
+        if (mana)
+        {
+            float fillAmount = (float)mana.Mana / (float)mana.maxMana;
+            image.fillAmount = fillAmount;
+            Color c = image.color;
+            c.r = c.g = c.b = fillAmount;
+            if (mana.Reloading)
+            {
+                c.a = 0.5f;
+            }
+            else
+            {
+                c.a = 1.0f;
+            }
+            image.color = c;
+        }
     }
 }
